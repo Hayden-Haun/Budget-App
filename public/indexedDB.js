c4f8dc97510ff03cc735af268a28c339c035a160
Hayden-Haun <haydenhaun@gmail.com>
2021-09-29 17:52:41 -0600

    updated updateDatabase function

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,5 +1,5 @@
 let db;
-const request = indexedDB.open("indexed-budget", 1);
+const request = indexedDB.open("budgetTracker", 1);
 
 request.onupgradeneeded = function (event) {
   const updatedDB = event.target.result;
@@ -14,12 +14,50 @@ request.onsucccess = function (event) {
   }
 };
 
+request.onerror = function (event) {
+  console.log("Error: " + event.target.errorCode);
+};
+
 //This is called in index.js in the CATCH of the POST request.
 function saveRecord(data) {
+  console.log("this is working");
+  console.log(data);
+
   const transaction = db.transaction(["pending"], "readwrite");
   const pendingStore = transaction.objectStore("pending");
 
   pendingStore.add(data);
-  console.log("this is working");
-  console.log("data");
 }
+
+function updateDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const pendingStore = transaction.objectStore("pending");
+
+  const getAll = pendingStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+          // access your pending object store
+          const pendingStore = transaction.objectStore("pending");
+          // clear all items in your store
+          pendingStore.clear();
+          location.reload();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", updateDatabase());
