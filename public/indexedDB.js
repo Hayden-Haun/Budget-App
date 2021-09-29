ab5c03017e61c6cfed254ec897e7a85a1603d60d
Hayden-Haun <haydenhaun@gmail.com>
2021-09-29 17:27:54 -0600

    updates to indexedDB.js, application is functioning correctly offline but console has errors

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,1 +1,25 @@
+let db;
+const request = indexedDB.open("indexed-budget", 1);
 
+request.onupgradeneeded = function (event) {
+  const updatedDB = event.target.result;
+  updatedDB.createObjectStore("pending", { autoincrement: true });
+};
+
+request.onsucccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.online) {
+    updateDatabase();
+  }
+};
+
+//This is called in index.js in the CATCH of the POST request.
+function saveRecord(data) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+
+  pendingStore.add(data);
+  console.log("this is working");
+  console.log("data");
+}
